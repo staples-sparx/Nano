e6f511762188b00981e1193740cbabe671d13f9e
Tim Brooks <tim.brooks@staples.com>
2014-09-30 16:40:48 -0700

    WIP: Continue implementing the servlet. Additionally, add ring-servlet utils dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]])
+                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
+                 [ring/ring-servlet "1.3.1"]])
--- src/nano/jetty.clj
+++ src/nano/jetty.clj
@@ -1,5 +1,24 @@
 (ns nano.jetty
-  (:import (org.eclipse.jetty.server Server)))
+  (:require [ring.util.servlet :as ring-servlet])
+  (:import (javax.servlet.http HttpServletResponse)
+           (org.eclipse.jetty.server Server ServerConnector Connector)
+           (org.eclipse.jetty.server.handler AbstractHandler)))
 
-(defn run-jetty [handler & {}]
-  )
+(set! *warn-on-reflection* true)
+
+(defn- build-handler [clojure-handler]
+  (proxy [AbstractHandler] []
+    (handle [_ base-request request response]
+      (try
+        (let [clojure-reponse (clojure-handler {})]
+          (ring-servlet/update-servlet-response base-request clojure-reponse))
+        (catch Exception e
+          )))))
+
+(defn run-jetty [handler & {:keys [port] :or {port 9000}}]
+  (let [server (Server.)
+        connector (doto ^Connector (ServerConnector. server) (.setPort port))
+        jetty-handler (build-handler handler)]
+    (doto server
+      (.addConnector connector)
+      (.setHandler jetty-handler))))
