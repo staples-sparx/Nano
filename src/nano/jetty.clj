dff31c35d76009a53f32ef5454ab2d4689b887b0
Tim Brooks <tim.brooks@staples.com>
2014-09-30 16:59:28 -0700

    Build smaller request map

--- src/nano/jetty.clj
+++ src/nano/jetty.clj
@@ -1,16 +1,22 @@
 (ns nano.jetty
   (:require [ring.util.servlet :as ring-servlet])
-  (:import (javax.servlet.http HttpServletResponse)
+  (:import (javax.servlet.http HttpServletResponse HttpServletRequest)
            (org.eclipse.jetty.server Server ServerConnector Connector)
            (org.eclipse.jetty.server.handler AbstractHandler)))
 
 (set! *warn-on-reflection* true)
 
+(defn request-map [^HttpServletRequest request]
+  {:remote-addr (.getRemoteAddr request)
+   :uri (.getRequestURI request)
+   :query-string (.getQueryString request)
+   :body (.getInputStream request)})
+
 (defn- build-handler [clojure-handler]
   (proxy [AbstractHandler] []
     (handle [_ base-request request response]
       (try
-        (let [clojure-reponse (clojure-handler {})]
+        (let [clojure-reponse (clojure-handler (request-map request))]
           (ring-servlet/update-servlet-response base-request clojure-reponse))
         (catch Exception e
           )))))
