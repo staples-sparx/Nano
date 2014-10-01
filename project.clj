78b3371136b708e81c5e6071179075562f2c7811
Tim Brooks <tim.brooks@staples.com>
2014-10-01 14:45:33 -0700

    Upgrade ring

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
                  [ring/ring-servlet "1.3.1"]
-                 [ring/ring-core "1.1.0"]])
+                 [ring/ring-core "1.3.1"]])
