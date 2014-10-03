8db9761dd84c488feabde94b0a2aa6cd1ff79446
Tim Brooks <tim.brooks@staples.com>
2014-10-03 11:42:41 -0700

    Make method public

--- src/nano/response.clj
+++ src/nano/response.clj
@@ -5,7 +5,7 @@
    :body (str "The requested URL does not exist: " url)
    :content-type "text/plain"})
 
-(defn- method-not-supported [method]
+(defn method-not-supported [method]
   {:status 405
    :body (str "The request method is not supported: " (name method))
    :content-type "text/plain"})
