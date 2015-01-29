7d64aeae3230d230b26e488e0ebba0f255695081
Tim Brooks <tim.brooks@staples.com>
2015-01-28 17:03:27 -0800

    Continue making the client more generic

--- src/nano/data/client.clj
+++ src/nano/data/client.clj
@@ -8,67 +8,78 @@
   (.submit ^ExecutorService threadpool
            ^Callable (cast Callable (request-fn))))
 
-(defn- try-request [url body deserialize-fn]
-  (try
-    (let [response (-> (client/post url {:body body})
-                       :body
-                       (deserialize-fn true))]
-      response)
-    (catch Exception e
-      (println e)
-      nil)))
+(defn- try-request
+  [url body {:keys [serialize-fn deserialize-fn exception-handler]}]
+  (if serialize-fn
+    (try
+      (let [response (-> (client/post url {:body (serialize-fn body)})
+                         :body
+                         deserialize-fn)]
+        response)
+      (catch Exception e
+        (if exception-handler (exception-handler e) (println e))
+        nil))
+    (try
+      (let [response (:body (client/post url {:form-params body
+                                              :content-type :json
+                                              :as :json}))]
+        response)
+      (catch Exception e
+        (if exception-handler (exception-handler e) (println e))
+        nil))))
 
-(defn try-request-repeatedly [url body pred deserialize-fn]
+(defn try-request-repeatedly [url body pred options]
   (loop [i 0]
     (if (== i 5)
       nil
-      (let [response (try-request url body deserialize-fn)]
+      (let [response (try-request url body options)]
         (if (pred response)
           response
           (do (Thread/sleep 500)
               (recur (inc i))))))))
 
-(defn- request-fn [url chunk-num datum serialize-fn deserialize-fn]
+(defn- request-fn [url chunk-num datum options]
   (fn []
     (try-request
       url
-      (serialize-fn {:chunk-number chunk-num :data datum})
-      deserialize-fn)))
+      {:chunk-number chunk-num :data datum}
+      options)))
 
 (defn- send-chunks
-  [url chunks indices serialize-fn deserialize-fn attempt-number]
-  (loop [indices indices attempt attempt-number]
+  [url chunks indices options]
+  (loop [indices indices attempt 0]
     (if (== 5 attempt)
       :failure/attempts-exhausted
       (let [fs (for [i (butlast indices)
                      :let [chunk (nth chunks i)]]
-                 (submit-fn (request-fn url i chunk serialize-fn deserialize-fn)))
+                 (submit-fn (request-fn url i chunk options)))
             final-chunk-num (last indices)
             final-chunk (nth chunks final-chunk-num)]
         (doseq [f fs] (deref f))
         (if-let [response (try-request-repeatedly
-                            url (serialize-fn {:data final-chunk
-                                               :chunk-number final-chunk-num
-                                               :final-chunk? true})
+                            url
+                            {:data final-chunk
+                             :chunk-number final-chunk-num
+                             :final-chunk? true}
                             (fn [response] (= (:message response)
                                               (format "Received chunk number %s."
                                                       final-chunk-num)))
-                            deserialize-fn)]
+                            options)]
           (if (not (empty? (:missing-chunks response)))
             (recur (:missing-chunks response) (inc attempt))
             :success)
           :failure/complete-reload)))))
 
-(defn- incremental-load-data [ip port key chunk-vec serialize-fn deserialize-fn]
+(defn- incremental-load-data [ip port key chunk-vec & options]
   (let [url (str "http://" ip ":" port "/data/" (name key))
         one-chunk? (== (count chunk-vec) 1)
         indices (range (count chunk-vec))]
     (if-not (try-request-repeatedly
               url
-              (serialize-fn {:chunk-number 0
-                             :data (first chunk-vec)
-                             :final-chunk? one-chunk?})
+              {:chunk-number 0
+               :data (first chunk-vec)
+               :final-chunk? one-chunk?}
               (fn [response] (= response {:message "Received chunk number 0."}))
-              deserialize-fn)
+              options)
       :failure/init-reload
-      (if one-chunk? :success (send-chunks url chunk-vec (rest indices) serialize-fn deserialize-fn 1)))))
+      (if one-chunk? :success (send-chunks url chunk-vec (rest indices) options)))))
