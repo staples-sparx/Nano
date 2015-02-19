0f781ccc8f0068664634729d44b647de99e07278
Tim Brooks <tim.brooks@staples.com>
2015-02-18 17:43:21 -0800

    Bump Nano version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.sparx/nano "0.5.2"
+(defproject org.sparx/nano "0.5.3"
   :description "A lightweight handler for Jetty 9"
   :url "https://github.com/staples-sparx/Nano"
   :license {:name "MIT License"
