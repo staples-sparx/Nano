f53330deadf262ec17bb9cd72a22328c381a4f0c
Tim Brooks <tim@uncontended.net>
2016-01-05 11:53:06 -0600

    Update versions of jetty

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.sparx/nano "0.5.4"
+(defproject org.sparx/nano "0.6.0-SNAPSHOT"
   :description "A lightweight handler for Jetty 9"
   :url "https://github.com/staples-sparx/Nano"
   :license {:name "MIT License"
             :url "http://mit-license.org/"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [cheshire "5.4.0"]]}}
-  :dependencies [[org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
-                 [ring/ring-servlet "1.3.1"]
+  :dependencies [[org.eclipse.jetty/jetty-server "9.3.6.v20151106"]
+                 [ring/ring-servlet "1.4.0"]
                  [bidi "1.12.0"]
                  [clj-http "1.0.1"]]
   :exclusions [org.clojure/clojure])
